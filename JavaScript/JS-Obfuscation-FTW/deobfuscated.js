81b2f90b6863c091270a086188a04803d89cb4b3
Rogue Blue <rogueblueprint@protonmail.com>
2023-03-14 01:07:38 -0700

    Added notes for last javascript challenge, writeup is not started yet

---
+++ JavaScript/JS-Obfuscation-FTW/deobfuscated.js
@@ -0,0 +1,8 @@
+if (document.getElementById("pass").value=="j00w1n")
+{
+    alert("You WIN!");window.location += "?lvl_password="+document.getElementById("pass").value
+}
+else
+{
+    alert("WRONG! Try again!")
+}
