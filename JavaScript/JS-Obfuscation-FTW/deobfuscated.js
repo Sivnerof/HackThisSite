2e0ae9396a3a3d3608953107264adf70e349822b
Rogue Blue <rogueblueprint@protonmail.com>
2023-03-14 14:17:08 -0700

    Added more notes to writeup for the last javascript challenge

--- JavaScript/JS-Obfuscation-FTW/deobfuscated.js
+++ JavaScript/JS-Obfuscation-FTW/deobfuscated.js
@@ -1,6 +1,7 @@
 if (document.getElementById("pass").value=="j00w1n")
 {
-    alert("You WIN!");window.location += "?lvl_password="+document.getElementById("pass").value
+    alert("You WIN!");
+    window.location += "?lvl_password="+document.getElementById("pass").value
 }
 else
 {
